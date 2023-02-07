7cbeb33198aa417e05965de29420c23becbae3b3
LasseDall <lass794rrstud.kea.dk>
2023-02-07 10:16:09 +0100

    First commit

---
+++ src/test/java/com/example/jirafrontend/JiraFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jirafrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraFrontendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
